748c76f006f9dc2f434ea220603373df8ad33d1a
Dina <dinarabea680@gmail.com>
2023-02-17 13:57:09 +0200

    login and registration form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Registration from './Pages/Registration/Registration';
+import Login from './Pages/Login/Login';
+
+function App() {
+  return (
+   <>
+    <Registration/>
+    <Login/>
+   </>
+  );
+}
+
+export default App;
